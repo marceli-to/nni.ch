e9b78ea20cbaadf4ed129a59e9b42b5755525609
Marcel Stadelmann <m@marceli.to>
2024-03-18 17:13:18 +0100

    wip

--- resources/js/modules/theme.js
+++ resources/js/modules/theme.js
@@ -1,49 +1,29 @@
 (function () {
-
   // Selectors
   const selectors = {
     theme: {
-      container: '[data-section-container]',
       header: 'header[data-icon-theme]',
       section: 'section[data-section-theme]',
-      icon: 'svg[data-icon-theme]',
     },
   };
 
-  // Debounce delay in milliseconds
-  const debounceDelay = 100;
-
   const init = () => {
-    if (document.querySelector(selectors.theme.container)) {
-      document.querySelector(selectors.theme.container).addEventListener('scroll', debouncedChange);
-    }
-  };
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          console.log(entry.target.getAttribute('data-section-theme'));
+          const theme = entry.target.getAttribute('data-section-theme');
+          const header = document.querySelector(selectors.theme.header);
+          header.setAttribute('data-icon-theme', theme);
+        }
+      });
+    }, {threshold: .5}); // Configure to trigger when 25% of the section is visible
 
-  const debounce = (func, wait) => {
-    let timeout;
-    return function executedFunction(...args) {
-      const later = () => {
-        clearTimeout(timeout);
-        func(...args);
-      };
-      clearTimeout(timeout);
-      timeout = setTimeout(later, wait);
-    };
-  };
-
-  const change = () => {
-    const sections = document.querySelectorAll(selectors.theme.section);
-    sections.forEach(section => {
-      if (section.getBoundingClientRect().top < window.innerHeight / 2) {
-        const theme = section.getAttribute('data-section-theme');
-        const header = document.querySelector(selectors.theme.header);
-        header.setAttribute('data-icon-theme', theme);
-      }
+    // Observe all sections
+    document.querySelectorAll(selectors.theme.section).forEach(section => {
+      observer.observe(section);
     });
   };
-
-  // Wrapped change function with debounce
-  const debouncedChange = debounce(change, debounceDelay);
 
   init();
 })();
