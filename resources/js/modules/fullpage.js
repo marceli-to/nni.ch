ba37663cafe3320cd4a46d26e3e24ddbe3ac3286
Marcel Stadelmann <m@marceli.to>
2024-05-08 19:25:25 +0200

    wip

--- resources/js/modules/fullpage.js
+++ resources/js/modules/fullpage.js
@@ -1,14 +1,17 @@
 import fullpage from 'fullpage.js';
 
 (function () {
-  const selector = '#fullpage';
 
   const init = () => {
+
+    const selector = '#fullpage';
 
     // check if the element exists
     if (!document.querySelector(selector)) {
       return;
     }
+
+    console.log('fullpage.js loaded');
 
     const fullPageInstance = new fullpage(selector, {
       licenseKey: 'O6CM9-7M5PI-Y4VK9-JVO3H-ZKKOM',
@@ -19,8 +22,6 @@ import fullpage from 'fullpage.js';
       scrollOverflowMacStyle: true,
       touchSensitivity: 20,
       credits: { enabled: false},
-      beforeLeave: function(origin, destination, direction, trigger){},
-      onLeave: function(origin, destination, direction, trigger){},
       afterLoad: function(origin, destination, direction, trigger){
         const nextSection = destination.item;
         const theme = nextSection.getAttribute('data-section-theme');
@@ -29,13 +30,6 @@ import fullpage from 'fullpage.js';
         if (!header) return;
         header.setAttribute('data-icon-theme', theme);
       },
-      afterRender: function(){},
-      afterResize: function(width, height){},
-      afterReBuild: function(){},
-      afterResponsive: function(isResponsive){},
-      afterSlideLoad: function(section, origin, destination, direction, trigger){},
-      onSlideLeave: function(section, origin, destination, direction, trigger){},
-      onScrollOverflow: function(section, slide, position, direction){}
     });
   };
 
