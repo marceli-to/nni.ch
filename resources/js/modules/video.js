98f3d8effcd765773048c3eb167303b6915f9ca9
Marcel Stadelmann <m@marceli.to>
2024-05-09 16:02:58 +0200

    wip

--- resources/js/modules/video.js
+++ resources/js/modules/video.js
@@ -8,58 +8,8 @@
   const breakpoint = 768;
 
   const init = () => {
-    lazyLoad(); // Initialize lazy loading
+    lazyLoad();
   }
-
-  // const init = () => {
-  //   // if no video elements are found, return
-  //   const videos = document.querySelectorAll(selectors.video);
-  //   if (videos.length === 0) {
-  //     return;
-  //   }
-  //   videos.forEach(video => {
-  //     video.setAttribute('data-current-source', 'low');
-  //   });
-  //   load();
-  //   window.addEventListener('resize', resize);
-  //   lazyLoad(); // Initialize lazy loading
-  // };
-
-  // const resize = () => {
-  //   const width = window.innerWidth;
-  //   load(width);
-  // };
-
-  // const load = (width = window.innerWidth) => {
-  //   const videos = document.querySelectorAll(selectors.video);
-
-  //   videos.forEach((video) => {
-  //     // get 'data-video-source' attribute
-  //     const sources = video.getAttribute('data-video-source');
-
-  //     // parse the source into an object
-  //     const parsedSources = JSON.parse(sources);
-
-  //     // get current source from the video's data attribute
-  //     let currentSource = video.getAttribute('data-current-source');
-  //     let source;
-
-  //     if (width < breakpoint && currentSource !== 'low') {
-  //       source = parsedSources.low;
-  //       video.setAttribute('data-current-source', 'low');
-  //     } 
-  //     else if (width >= breakpoint && currentSource !== 'high') {
-  //       source = parsedSources.high;
-  //       video.setAttribute('data-current-source', 'high');
-  //     } else {
-  //       // No source change needed
-  //       return;
-  //     }
-
-  //     video.src = `${source}`;
-  //     video.classList.remove('hidden');
-  //   });
-  // };
 
   const lazyLoad = () => {
     let lazyVideos = [...document.querySelectorAll(selectors.lazy)];
