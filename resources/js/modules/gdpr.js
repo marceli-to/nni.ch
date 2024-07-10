8eab09d685d35ef6169f3e84fd4d01cf3a191ba1
Marcel Stadelmann <m@marceli.to>
2024-07-10 09:30:33 +0200

    wip

--- resources/js/modules/gdpr.js
+++ resources/js/modules/gdpr.js
@@ -1,4 +1,3 @@
-
 (function () {
 
   const selectors = {
--- resources/js/modules/slides.js
+++ resources/js/modules/slides.js
@@ -1,30 +1,31 @@
-(function() {
-  // Select all articles (slides)
-  const slides = document.querySelectorAll('[data-slide]');
-  // Select all navigation links
-  const navLinks = document.querySelectorAll('[data-slides-nav] a');
+(function () {
 
-  // Function to show a specific slide
-  function showSlide(index) {
-    slides.forEach((slide, i) => {
-      if (i === index) {
-        slide.classList.remove('hidden');
-        slide.classList.add('visible');
-      } else {
-        slide.classList.add('hidden');
-        slide.classList.remove('visible');
-      }
+  const selectors = {
+    slides: '[data-slide]',
+    slideButtons: '[data-slides-nav] a',
+  };
+
+  const init = () => {
+    const buttons = document.querySelectorAll(selectors.slideButtons);
+    if (buttons) {
+      buttons.forEach((button) => {
+        button.addEventListener('click', handleClick);
+      });
+    }
+  };
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    const slug = e.currentTarget.dataset.slideButton;
+    const slides = document.querySelectorAll(selectors.slides);
+    slides.forEach((slide) => {
+      slide.classList.add('hidden');
     });
-  }
+    // show the clicked slide
+    const clickedSlide = document.querySelector(`[data-slide="${slug}"]`);
+    clickedSlide.classList.remove('hidden');
+    clickedSlide.classList.add('visible');
+  };  
 
-  // Add click event listeners to navigation links
-  navLinks.forEach((link, index) => {
-    link.addEventListener('click', function(e) {
-      e.preventDefault();
-      showSlide(index + 1); // +1 because the first slide is not in the nav
-    });
-  });
-
-  // Initialize: show the first slide
-  showSlide(0);
+  init();
 })();
